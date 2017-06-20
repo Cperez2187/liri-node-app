37711b57a955d749d1dacb2473607be08c12465d
cperez2187 <cperez2187@gmail.com>
2017-06-20 16:45:16 -0700

    Added Twitter, Spotify, & OMDB keys

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'KQmlORcJT9WL8JmtXkHfjagEP',
+  consumer_secret: '6qEIe3ijPk0wFc9iIUYTzou0M0PG9BVXhLCujj73JxyLVfr5gc',
+  access_token_key: 'LnM4dBNHmj7Gu7AbNdXhlwPOmPpyRlheUXulEipHCRxPa',
+}
+
+exports.spotifyKeys = {
+	client_ID: 'bfb206c7cad04aa3a3d6b181dc9d03fb',
+	client_secret: '2f33812374aa49878f91a390c6a2714d'
+}
+
+exports.omdbKey = {
+	api_key: '40e9cece'
+}
---
+++ liri.js
@@ -0,0 +1,2 @@
+// Get twitter keys data
+const twitter_keys = require('keys.js');
